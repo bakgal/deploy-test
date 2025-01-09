c08509892540980891215c7825a44f3bb4b10662
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-01-09 20:05:39 +0900

    refactor: health check 메시지 변경

--- src/main/kotlin/io/demo/deploytest/HealthController.kt
+++ src/main/kotlin/io/demo/deploytest/HealthController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class HealthController {
     @GetMapping("/health")
     fun healthCheck(): String {
-        return "ok"
+        return "ok, this app is health"
     }
 }
